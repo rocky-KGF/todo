b712a0e289a41691c4c93fba474c3e4d5ca85784
bsk-123 <bhanusampath321@gmail.com>
2020-11-27 13:22:20 +0530

    Add gitignore, procfile and updated ports

--- server.js
+++ server.js
@@ -130,7 +130,12 @@ app.post("/:type",function(req,res){
     }
 })
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
 
-app.listen("3000",function(){
+app.listen(port,function(){
     console.log("server strted at 3000")
 })
